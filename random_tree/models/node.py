320ffdcb2a19af76e3aae2cb96a6f1fc5041188f
amtlib <grzegorz.andrzej.pach@gmail.com>
2020-03-08 01:30:38 +0100

    Init random tree solution with Node class

---
+++ random_tree/models/node.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.value = None
+        self.sum = 0
+        self.count = 0
+        self.current_median = None
+        self.median_value_before = None
+        self.median_value_after = None
