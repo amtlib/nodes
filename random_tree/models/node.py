0d6827b663785994ed40a3ff4b9ec5fb90b557e3
amtlib <grzegorz.andrzej.pach@gmail.com>
2020-03-10 00:18:50 +0100

    Add functions to set/get current sum

--- random_tree/models/node.py
+++ random_tree/models/node.py
@@ -1,11 +1,26 @@
 class Node:
     def __init__(self):
-        self.parent = None
-        self.left = None
-        self.right = None
-        self.value = None
-        self.sum = 0
-        self.count = 0
-        self.current_median = None
-        self.median_value_before = None
-        self.median_value_after = None
+        self.__parent = None
+        self.__left = None
+        self.__right = None
+        self.__value = None
+        self.__sum = 0
+        self.__count = 0
+        self.__current_median = None
+        self.__median_value_before = None
+        self.__median_value_after = None
+
+    def __add_to_sum(self, value):
+        if self.__parent:
+            self.__parent.__add_to_sum()
+        self.__sum += value
+    
+    def __sub_from_sum(self, value):
+        if self.__parent:
+            self.__parent.__sub_from_sum(value)
+        self.__sum -= value
+
+    def get_sum(self):
+        return self.__sum
+
+    
